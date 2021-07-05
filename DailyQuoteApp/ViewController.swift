6cba939a4b10d181cc332911fb8533f1bdb8a469
Malik Farhan Asim <farhan.asim46@gmail.com>
2021-07-05 20:49:29 +0500

    Initial Commit

---
+++ DailyQuoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyQuoteApp
+//
+//  Created by Malik Farhan Asim on 05/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
